2dd12fc6974029b6fd2338b05fa990e9bc3404b1
Sharu2172 <57060263+Sharu2172@users.noreply.github.com>
2021-06-07 11:29:14 +0530

    update

--- config.php
+++ config.php
@@ -7,8 +7,7 @@ $name = "IMS";
 $conn = mysqli_connect($host, $user, $pass, $name); //host_name,username,password,Database_name
 //check connection
 if (!$conn) {
-    echo "connection failed: " . mysqli_connect_error();
-    echo `<script>
-    console.log("Connection failed");
-    </script>`;
+    echo "<script>console.log('Connection Failed'); </script>";
+} else {
+    echo "<script>console.log('Connection Sucessful'); </script>";
 }
